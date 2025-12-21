819c09f829d43c803ab72046b737b6c6d620615c
vinayaka <vinayaka@email.com>
2025-12-21 11:15:13 +0530

    blogs and search fields

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -19,10 +19,13 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path,include
 from . import views
+from blogs import views as blogs_view
 import blogs
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home"),
     path('category/',include("blogs.urls")),
+    path('<slug:blog_slug>',blogs_view.single_blog_page,name="blogs"),
+    path('blogs/search/',blogs_view.search,name="search"),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Category,Blog
+from django.db.models import Q
 
 # Create your views here.
 def posts_by_category(request,category_id):
@@ -14,3 +15,19 @@ def posts_by_category(request,category_id):
         "category":category,
     }
     return render(request,"posts_by_category.html",context)
+
+def single_blog_page(request,blog_slug):
+    single_blog = get_object_or_404(Blog,slug=blog_slug,status="Published")
+    context={
+        "single_blog":single_blog
+    }
+    return render(request,"blogs.html",context)
+
+def search(request):
+    keyword=request.GET.get('keyword')
+    search_result=Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword) , status="Published" )
+    context={
+        "search_result":search_result,
+        "keyword":keyword,
+    }
+    return render(request,'search.html',context)
